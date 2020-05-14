e8bf18b9e31bfe71433e47129ee6f287fe06a1dd
i-Apoorva <apoorva.ambhoj95@gmail.com>
2020-05-14 13:06:30 +0530

    image upload and rendering option

--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,10 @@ const UserSchema= new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }], 
+    avatar: {
+        type: Buffer
+    }
 }, { 
     timestamps: true
 })
@@ -64,6 +67,7 @@ UserSchema.methods.toJSON = function (){
     
     delete userObject.password
     delete userObject.tokens
+    delete userObject.avatar
 
     return userObject
 }
