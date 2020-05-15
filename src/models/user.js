85c3973ad0fabca845e21dae855ec685f82c874d
i-Apoorva <apoorva.ambhoj95@gmail.com>
2020-05-15 20:36:15 +0530

    SendGrid Integration

--- src/models/user.js
+++ src/models/user.js
@@ -74,7 +74,7 @@ UserSchema.methods.toJSON = function (){
 
 UserSchema.methods.generateAuthToken = async function() {
     const user= this
-    const token = await jwt.sign({_id: user._id.toString()}, 'thisismysecret')
+    const token = await jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({token})
     await user.save()
     return token
